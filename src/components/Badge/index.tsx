8f4c93fa226f44705f3251319e939db6c9604d0d
kimhyerin0909 <khyerin0909@gmail.com>
2023-04-28 19:42:38 +0900

    CHORE :: 필요없는 코드 삭제

--- src/components/Badge/index.tsx
+++ src/components/Badge/index.tsx
@@ -2,13 +2,11 @@ import React from "react";
 import styled from "@emotion/styled";
 import palette from "../../theme/palette";
 import Text from "../Text";
-import { IconNameType } from "../../utils/stories";
 
 interface BadgeProps {
   text?: string;
   mode?: "Notification" | "Tag";
   children?: any;
-  iconName?: IconNameType;
 }
 
 export const Badge = ({ mode = "Tag", text = "Default", children }: BadgeProps) => {
