a621d6abbc93f2584cae96ad96d4b4c4fa0be650
kimhyerin0909 <khyerin0909@gmail.com>
2023-04-28 09:34:48 +0900

    ADD :: icon color

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,16 +1,51 @@
 import React, { createElement } from "react";
-import { IconNameType, IconSizeType, getIconComponentFromName } from "../../utils/stories";
+import {
+  IconNameType,
+  IconSizeType,
+  PaletteType,
+  getColorFromName,
+  getIconComponentFromName,
+} from "../../utils/stories";
 import { iconSize } from "../../theme/size";
+import styled from "@emotion/styled";
 
 interface IconProps {
-  iconName: IconNameType;
+  iconName?: IconNameType;
   size?: IconSizeType;
+  strokeColor?: PaletteType;
+  fillColor?: PaletteType;
 }
 
-export const Icon = ({ iconName = "BSMLogo", size = "MEDIUM" }: IconProps) => {
+export const Icon = ({
+  iconName = "BSMLogo",
+  size = "MEDIUM",
+  fillColor,
+  strokeColor,
+}: IconProps) => {
   return (
-    <div style={{ width: `${iconSize[size]}px`, objectFit: "contain" }}>
+    <IconStyle
+      size={size}
+      strokeColor={getColorFromName(strokeColor)}
+      fillColor={getColorFromName(fillColor)}>
       {createElement(getIconComponentFromName(iconName))}
-    </div>
+    </IconStyle>
   );
 };
+
+export const IconStyle = styled.div<{
+  size: IconSizeType;
+  fillColor?: string;
+  strokeColor?: string;
+}>`
+  display: flex;
+  align-items: center;
+
+  width: ${(props) => iconSize[props.size]}px;
+  object-fit: contain;
+  svg {
+    path {
+      stroke: ${(props) => props.strokeColor};
+      fill: ${(props) => props.fillColor};
+    }
+  }
+`;
