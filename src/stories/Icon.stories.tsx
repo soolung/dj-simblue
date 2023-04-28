3c2274babf64ea39b1beaf60056bae8437a8436f
kimhyerin0909 <khyerin0909@gmail.com>
2023-04-28 09:43:00 +0900

    CHORE :: delete list

--- src/stories/Icon.stories.tsx
+++ src/stories/Icon.stories.tsx
@@ -1,9 +1,5 @@
 import type { StoryObj } from "@storybook/react";
 import { Icon } from "../components/Icon";
-import { iconNames } from "../utils/stories";
-import styled from "@emotion/styled";
-import Text from "../components/Text";
-import typo from "../theme/typo";
 
 type Story = StoryObj<typeof Icon>;
 
@@ -15,19 +11,3 @@ export default {
 export const Default: Story = {
   render: (args) => <Icon {...args} />,
 };
-
-const IconList = styled.div`
-  display: flex;
-  gap: 20px;
-`;
-
-export const List = () => (
-  <>
-    <Text typo={typo.LABEL.MEDIUM}>{iconNames.length} icons</Text>
-    <IconList>
-      {iconNames.map((name) => (
-        <Icon iconName={name} size="LARGE" />
-      ))}
-    </IconList>
-  </>
-);
