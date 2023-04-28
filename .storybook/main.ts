682dcbe8a1e99a15695d6a3e75eb364a149ab0bb
kimhyerin0909 <khyerin0909@gmail.com>
2023-04-28 09:33:12 +0900

    CHORE :: storybook color picker

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,6 +6,7 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
     "@storybook/addon-interactions",
+    "storybook-color-picker",
   ],
   framework: {
     name: "@storybook/react-webpack5",
