1c85ddfb7b926dddfe28f1fe1b16d980a98f0fdc
kimhyerin0909 <khyerin0909@gmail.com>
2023-05-01 15:02:25 +0900

    FIX :: svg 파일 안 보임

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -12,40 +12,18 @@ import styled from "@emotion/styled";
 interface IconProps {
   iconName?: IconNameType;
   size?: IconSizeType;
-  strokeColor?: PaletteType;
-  fillColor?: PaletteType;
 }
 
-export const Icon = ({
-  iconName = "BSMLogo",
-  size = "MEDIUM",
-  fillColor,
-  strokeColor,
-}: IconProps) => {
-  return (
-    <IconStyle
-      size={size}
-      strokeColor={getColorFromName(strokeColor)}
-      fillColor={getColorFromName(fillColor)}>
-      {createElement(getIconComponentFromName(iconName))}
-    </IconStyle>
-  );
+export const Icon = ({ iconName = "BSMLogo", size = "MEDIUM" }: IconProps) => {
+  return <IconStyle size={size}>{createElement(getIconComponentFromName(iconName))}</IconStyle>;
 };
 
 export const IconStyle = styled.div<{
   size: IconSizeType;
-  fillColor?: string;
-  strokeColor?: string;
 }>`
   display: flex;
   align-items: center;
 
   width: ${(props) => iconSize[props.size]}px;
   object-fit: contain;
-  svg {
-    path {
-      stroke: ${(props) => props.strokeColor};
-      fill: ${(props) => props.fillColor};
-    }
-  }
 `;
