7288e48007b1c43e61b1b189d26792fb4fffce24
kimhyerin0909 <khyerin0909@gmail.com>
2023-05-15 14:04:09 +0900

    FIX :: checkbox icon

--- src/components/Inputs/Checkbox/index.tsx
+++ src/components/Inputs/Checkbox/index.tsx
@@ -10,7 +10,7 @@ export const Checkbox = () => {
     <CheckboxStyle
       state={isCheck}
       onClick={() => setIsCheck((prev) => (prev === "CHECKED" ? "UNCHECKED" : "CHECKED"))}>
-      <Icon iconName="Check" />
+      <Icon size="LARGE" color="MONO_WHITE" iconName="CheckLine" />
     </CheckboxStyle>
   );
 };
--- src/theme/icon.ts
+++ src/theme/icon.ts
@@ -1,6 +1,7 @@
 export { ReactComponent as ArrowBack } from "../assets/icon/arrow_back.svg";
 export { ReactComponent as ArrowDown } from "../assets/icon/arrow_down.svg";
 export { ReactComponent as ArrowFront } from "../assets/icon/arrow_front.svg";
+export { ReactComponent as CheckLine } from "../assets/icon/check-line.svg";
 export { ReactComponent as Check } from "../assets/icon/check.svg";
 export { ReactComponent as Close } from "../assets/icon/close.svg";
 export { ReactComponent as Cloud } from "../assets/icon/cloud.svg";
