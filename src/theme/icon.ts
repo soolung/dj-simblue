7f58b6d4babf4d83e38ec02e7e50c82b8ad1975e
kimhyerin0909 <khyerin0909@gmail.com>
2023-09-05 15:45:08 +0900

    ADD :: new icons (#8

--- src/theme/icon.ts
+++ src/theme/icon.ts
@@ -1,17 +1,19 @@
+export { ReactComponent as Add } from "../assets/icon/add.svg";
 export { ReactComponent as ArrowBack } from "../assets/icon/arrow_back.svg";
 export { ReactComponent as ArrowDown } from "../assets/icon/arrow_down.svg";
 export { ReactComponent as ArrowFront } from "../assets/icon/arrow_front.svg";
+export { ReactComponent as BSM } from "../assets/icon/bsm.svg";
 export { ReactComponent as CheckLine } from "../assets/icon/check-line.svg";
 export { ReactComponent as Check } from "../assets/icon/check.svg";
 export { ReactComponent as Close } from "../assets/icon/close.svg";
 export { ReactComponent as Cloud } from "../assets/icon/cloud.svg";
+export { ReactComponent as Copy } from "../assets/icon/copy.svg";
 export { ReactComponent as Edit } from "../assets/icon/edit.svg";
 export { ReactComponent as Error } from "../assets/icon/error.svg";
+export { ReactComponent as Excel } from "../assets/icon/excel.svg";
+export { ReactComponent as Google } from "../assets/icon/google.svg";
 export { ReactComponent as Help } from "../assets/icon/help.svg";
 export { ReactComponent as Lock } from "../assets/icon/lock.svg";
 export { ReactComponent as Person } from "../assets/icon/person.svg";
 export { ReactComponent as Search } from "../assets/icon/search.svg";
-
-export { ReactComponent as BSM } from "../assets/icon/bsm.svg";
-export { ReactComponent as Excel } from "../assets/icon/excel.svg";
-export { ReactComponent as Google } from "../assets/icon/google.svg";
+export { ReactComponent as User } from "../assets/icon/user.svg";
